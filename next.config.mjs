0c85bb2ee31c107c118d53930b34082ba6ccac0b
Ozsumit <oniwagon>
2024-10-10 18:23:30 +0545

    "Updated next.config.mjs to add PWA configuration and caching, and modified public/sw.js to update cacheWillUpdate function"

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,69 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace with your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+// @ts-check
+
+import withPWA from "next-pwa";
+
+//  import('next').NextConfig
+
+const runtimeCaching = [
+  {
+    urlPattern: /^https?.*/, // Cache all requests to external APIs
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "external-cache",
+      expiration: {
+        maxEntries: 50,
+        maxAgeSeconds: 24 * 60 * 60, // 1 day
       },
-    ];
+    },
   },
-  // Disable static optimization
-  unstable_runtimeJS: true,
-  unstable_JsPreload: false,
-  // Force server-side rendering
-  unstable_useServerComponents: true,
-  // Existing configurations
+  {
+    urlPattern: "/",
+    handler: "NetworkFirst", // Serve from network first, fallback to cache
+    options: {
+      cacheName: "homepage-cache",
+      expiration: {
+        maxEntries: 1,
+        maxAgeSeconds: 24 * 60 * 60, // 1 day
+      },
+    },
+  },
+  {
+    urlPattern: /\/_next\/image/,
+    handler: "CacheFirst", // Cache optimized images
+    options: {
+      cacheName: "image-cache",
+      expiration: {
+        maxEntries: 100,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+      },
+    },
+  },
+  {
+    urlPattern: /\/page.tsx/,
+    handler: "CacheFirst", // Offline fallback page
+    options: {
+      cacheName: "offline-cache",
+      expiration: {
+        maxEntries: 1,
+      },
+    },
+  },
+];
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // output: "export",
+  // Add other Next.js config options here
 };
 
+const withPWAConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
 export default nextConfig;
+
+
+// export default withPWAConfig(nextConfig);gfh
